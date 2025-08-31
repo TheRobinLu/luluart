171cf2ec4417279f07a75a9c50347529e75e63a2
TheRobinLu <lu.robin@hotmail.com>
2025-08-31 10:27:28 -0400

    test repo

--- constants/const.ts
+++ constants/const.ts
@@ -1,5 +1,6 @@
-export const Version = "Version: 0.06.03";
+export const Version = "Version: 0.06.04";
 export const ReleaseNote =
+	"2025-08-31 0.06.04 test repo\n" +
 	"2025-08-17 0.06.03 editArea layout \n" +
 	"2025-08-16 0.06.02 Adding new background editor features\n" +
 	"2025-08-15 0.06.01 Adding AI editing tools on UI\n" +
